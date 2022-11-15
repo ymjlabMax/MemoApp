b25fd6397b01cf294c339183668887c4bebde8e4
Max <sud665@ymjlab.com>
2022-11-15 13:51:03 +0900

    초기세팅

---
+++ app/src/main/java/com/example/mymemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mymemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button write_btn;
+    private final int REQUEST_TEST = 200;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        write_btn = findViewById(R.id.write_btn);
+        write_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), WriteActivity.class);
+                startActivityForResult(i, REQUEST_TEST);
+            }
+        });
+
+
+    }
+
+
+
+
+}
