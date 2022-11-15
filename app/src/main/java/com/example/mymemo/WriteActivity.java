bf394ba546322a3411d64d959aa01b1d55b31a6e
Max <sud665@ymjlab.com>
2022-11-15 15:04:12 +0900

    글쓰기 기능 구현

--- app/src/main/java/com/example/mymemo/WriteActivity.java
+++ app/src/main/java/com/example/mymemo/WriteActivity.java
@@ -2,10 +2,15 @@ package com.example.mymemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WriteActivity extends AppCompatActivity {
 
@@ -34,6 +39,31 @@ public class WriteActivity extends AppCompatActivity {
             public void onClick(View view) {
                 finish();
 
+            }
+        });
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String edit_title = title.getText().toString();
+                String edit_content = content.getText().toString();
+                String save_form =  "{\"title\":\""+edit_title+"\",\"content\":\""+edit_content+"\"}";
+
+                Long now = System.currentTimeMillis();
+                Date mDate = new Date(now);
+                SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String getTime = simpleDate.format(mDate).toString();
+
+                Log.d("WriteActivity","제목 : "+edit_title+", 내용 : "+edit_content+", 현재시간 : "+getTime);
+                PreferenceManager.setString(getApplication(), getTime, save_form);
+
+
+                Intent intent = new Intent();
+                intent.putExtra("date", getTime);
+                intent.putExtra("title", edit_title);
+                intent.putExtra("content", edit_content);
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
 
